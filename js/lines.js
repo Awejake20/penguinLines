f27b8e01f7f73fd23600d2ca51060a0ea6d03252
Jake Pfaller <pfaller.jake@gmail.com>
2020-09-28 10:39:49 -0400

    Line Graph Lab

--- js/lines.js
+++ js/lines.js
@@ -8,7 +8,64 @@
 var drawLines = function(penguins,target,
                          xScale,yScale)
 {
+   var lineGenerator = d3.line()
+            .x(function(quiz,i)
+            {
+                return xScale(i)
+            })
+            .y(function(quiz)
+            {
+                return yScale(quiz.grade)
+            })
    
+   var lines = d3.select("svg")
+   .select("#graph")
+   .selectAll("g")
+   .data(penguins)
+   .enter()
+   .append("g")
+   .classed("line",true)
+   .on("mouseover",function(penguins)
+    {   
+            if(! d3.select(this).classed("off"))
+            {
+            d3.selectAll(".line")
+            .classed("selected path",false);
+            
+            d3.select(this)
+                .classed("selected path",true)
+                .raise(); //move to top
+                
+            var xPos = d3.event.pageX;
+            var yPos = d3.event.pageY;
+      
+            d3.select("#tooltip")
+            .classed("hidden",false)
+            .style("top",yPos+"px")
+            .style("left",xPos+"px")
+            .select("img")
+            .attr("src","../imgs/"+penguins.picture)
+            }
+    })
+   .on("mouseout",function(penguins)
+           {
+            if(! d3.select(this).classed("off"))
+            {
+            
+            d3.selectAll(".line")
+                .classed("selected path",false);
+            d3.select("#tooltip")
+                .classed("hidden",true)
+            }
+            
+        })
+   
+   lines.append("path")
+    .datum(function(penguins)
+           {
+            return penguins.quizes
+            })
+   .attr("d",lineGenerator)
 }
 
 
@@ -24,8 +81,17 @@ var makeTranslateString = function(x,y)
 var drawAxes = function(graphDim,margins,
                          xScale,yScale)
 {
-   
- 
+   var xAxis=d3.axisBottom(xScale)
+   d3.select("svg")
+    .append("g")
+    .call(xAxis)
+    .attr("transform", makeTranslateString(margins.left, graphDim.height+margins.top))
+    
+    var yAxis = d3.axisLeft(yScale)
+    d3.select("svg")
+    .append("g")
+    .attr("transform", makeTranslateString(margins.left, margins.top))
+    .call(yAxis)
 }
 
 
@@ -33,7 +99,31 @@ var drawAxes = function(graphDim,margins,
 //margins - object that stores the size of the margins
 var drawLabels = function(graphDim,margins)
 {
+    var title = d3.select("svg")
+   
+    title.append("text")
+    .text("Class Quiz Grades Over Time")
+    .classed("title", true)
+    .attr("text-anchor","middle")
+    .attr("x",margins.left+(graphDim.width/2))
+    .attr("y",margins.top/2)
     
+    var xLabel = d3.select("svg")
+    
+    xLabel.append("text")
+    .text("Quiz Number")
+    .classed("label", true)
+    .attr("text-anchor","middle")
+    .attr("x",margins.left+(graphDim.width/2))
+    .attr("y",graphDim.height+margins.top+margins.bottom)
+    
+    var yLabel = d3.select("svg")
+    yLabel.append("text")
+    .attr("transform",makeTranslateString(margins.left/2, margins.top+graphDim.height/2 ) + " rotate(-90)")
+    .text("Grade")
+    .classed("label","true")
+    .attr("text-anchor","middle")
+    //.attr("transform", "rotate(-90)")
 }
 
 
@@ -45,7 +135,7 @@ var initGraph = function(penguins)
     //size of screen
     var screen = {width:800,height:600}
     //how much space on each side
-    var margins = {left:30,right:20,top:20,bottom:30}
+    var margins = {left:30,right:20,top:40,bottom:30}
     
     
     
@@ -62,7 +152,7 @@ var initGraph = function(penguins)
     
     var target = d3.select("svg")
     .append("g")
-    .attr("id","#graph")
+    .attr("id","graph")
     .attr("transform",
           "translate("+margins.left+","+
                         margins.top+")");
@@ -72,7 +162,7 @@ var initGraph = function(penguins)
                         {return quiz.day});
     
     var xScale = d3.scaleLinear()
-        .domain([1,maxDay])
+        .domain([0,maxDay])
         .range([0,graph.width])
 
     var yScale = d3.scaleLinear()
